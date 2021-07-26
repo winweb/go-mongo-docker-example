60bcbb9aef2693c4c147b1d64ef37364a836599a
winweb <theerasun@gmail.com>
2021-07-26 21:01:39 +0700

    add function for update and delete data.

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 </p>
 
 This is the underlying code for [How to Keep Your Development Environment Clean](https://outcrawl.com/clean-development-environment-docker/) article.
+
+
+# After I forked these project. I added fuction for delete and update data.
+
+## Docker
+```sh
+$ docker-compose up
+```
--- api/main.go
+++ api/main.go
@@ -12,11 +12,13 @@ import (
 
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Post struct {
-	Text      string    `json:"text" bson:"text"`
-	CreatedAt time.Time `json:"createdAt" bson:"created_at"`
+	Id        bson.ObjectId `bson:"_id" json:"id"`
+	Text      string        `json:"text" bson:"text"`
+	CreatedAt time.Time     `json:"createdAt" bson:"created_at"`
 }
 
 var posts *mgo.Collection
@@ -33,7 +35,7 @@ func main() {
 	session.SetMode(mgo.Monotonic, true)
 
 	// Get posts collection
-	posts = session.DB("app").C("posts")
+	posts = session.DB("app").C("posts2")
 
 	// Set up routes
 	r := mux.NewRouter()
@@ -41,6 +43,10 @@ func main() {
 		Methods("POST")
 	r.HandleFunc("/posts", readPosts).
 		Methods("GET")
+	r.HandleFunc("/posts/{id}", updatePost).
+		Methods("PUT")
+	r.HandleFunc("/posts/{id}", deletePost).
+		Methods("DELETE")
 
 	http.ListenAndServe(":8080", cors.AllowAll().Handler(r))
 	log.Println("Listening on port 8080...")
@@ -61,6 +67,7 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 		responseError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	post.Id = bson.NewObjectId()
 	post.CreatedAt = time.Now().UTC()
 
 	// Insert new post
@@ -90,4 +97,45 @@ func responseError(w http.ResponseWriter, message string, code int) {
 func responseJSON(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
+}
+
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	//get id from incoming url
+	vars := mux.Vars(r)
+	id := bson.ObjectIdHex(vars["id"])
+
+	//decode the incoming Note into json
+	var postResource Post
+	err = json.NewDecoder(r.Body).Decode(&postResource)
+	if err != nil {
+		panic(err)
+	}
+
+	//partial update on mongodb
+	err = posts.Update(bson.M{"_id": id},
+		bson.M{"$set": bson.M{
+			"text": postResource.Text,
+		}})
+
+	if err == nil {
+		log.Printf("Updated Post : %s", id, postResource.Text)
+	} else {
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	err := posts.Remove(bson.M{"_id": bson.ObjectIdHex(id)})
+	if err != nil {
+		log.Printf("Could not find the Note %s to delete", id)
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
